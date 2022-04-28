68fb30f86d4bf32fec7e103fea82a457e2a4cd4f
Alex Kwiatkowski <alex+git@fremantle.io>
2022-04-27 22:14:21 -0700

    release v0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OrderedNaryTree.MixProject do
   def project do
     [
       app: :ordered_nary_tree,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.11",
       package: package(),
       start_permanent: Mix.env() == :prod,
